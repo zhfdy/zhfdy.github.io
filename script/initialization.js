0f0a2cd474265c47efaea060fc183ce89efa172c
zhfdy <148465741+zhfdy@users.noreply.github.com>
2024-07-26 02:49:18 +0800

    Update initialization.js

--- script/initialization.js
+++ script/initialization.js
@@ -12,14 +12,15 @@
     const game_box = document.createElement('div')
     game_box.classList.add('game_box')
     document.body.appendChild(game_box)
-    game_box.innerHTML = `${name}游戏初始化成功`
+    const game_content = document.createElement('div')
+    game_content.classList.add('game_content')
+    game_content.textContent = `${name}`
+    document.body.appendChild(game_content)
 
     //增加控件菜单
-    function add_list() {
-        const controls = document.createElement('ul')
-        controls.classList.add('controls')
-        game_box.appendChild(controls)
-    }
+    const controls = document.createElement('ul')
+    controls.classList.add('controls')
+    game_box.appendChild(controls)
 
     //控件点击事件，实现下拉
     const control_btn = document.createElement('div')
@@ -29,11 +30,17 @@
         const controls = document.querySelector('.controls')
         this.classList.contains(`controls_btn_off`)
             ? (this.classList.remove(`controls_btn_off`)
-                , controls.remove())
+                , controls.classList.remove(`controls_on`))
             : (this.classList.add(`controls_btn_off`)
-                , add_list()
-                , add_li())
+                , controls.classList.add(`controls_on`))
     })
+    document.body.addEventListener('click', function (e) {
+        if (e.target === control_btn || e.target === controls) return
+        if (!(document.querySelector('.controls_btn').classList.contains(`controls_btn_off`))) return
+        controls.classList.remove(`controls_on`)
+        control_btn.classList.remove(`controls_btn_off`)
+    })
+
 
 
     //添加控件项
@@ -50,14 +57,25 @@
         }
         //暗夜模式切换
         const dark = add_control('dark_mode', '深色')
+        document.querySelector('.control_dark_mode').addEventListener('click', function () {
+            this.classList.contains('dark_mode_off') ? document.querySelector('.game_box').classList.add('dark_mode_on') : document.querySelector('.game_box').classList.remove('dark_mode_on')
+        })
         //声音开关
         const sound = add_control('sound_on', '声音')
         //返回按钮
         const back = add_control('back', '返回')
-        //退出按钮
-        const exit = add_control('exit', '退出')
+        document.querySelector('.control_back').addEventListener('click', function () {
+            window.history.back()
+        })
+        //开挂按钮
+        const exit = add_control('exit', '开挂')
+        document.querySelector('.control_exit').addEventListener('click', function () {
+            myMarkNum = 999
+            document.querySelector('.mark').textContent = `分数：${myMarkNum}`
+        })
     }
-    
+    add_li()
+
 
     //插入对应css/js文件
     const link = document.createElement('link')
