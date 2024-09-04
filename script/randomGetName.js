217569876e6a5f6c0a4808fe8dc3905f9d2d2403
zhfdy <148465741+zhfdy@users.noreply.github.com>
2024-09-04 17:26:56 +0800

    更新 randomGetName.js

--- script/randomGetName.js
+++ script/randomGetName.js
@@ -1,17 +1,40 @@
 
 export function madeTitle() {
-    const times = new Date().toLocaleTimeString()
-    return times
+    const times = new Date().toLocaleTimeString()
+    return times
 }
 
 export async function randomGetName() {
-    try {
-        const response = await fetch('../data/people.json')
-        const data = await response.json()
-        const randomNum = Math.floor(Math.random() * data.people.length)
-        return data.people[randomNum]
-    } catch (error) {
-        console.error('Error fetching or processing data:', error)
-        throw error
-    }
+    try {
+        function onloadImg(imgsUrl, index = 0){
+            if(index >= imgsUrl.length) return
+        
+            const img = new Image()
+            img.src = `../image/${imgsUrl[index]}`
+        
+            img.onload = () => {
+                onloadImg(imgsUrl, index + 1)
+            }
+
+            img.onerror = () => {
+                onloadImg(imgsUrl, index + 1)
+            }
+        }
+        
+        const response = await fetch('../data/people.json')
+        const data = await response.json()
+        const randomNum = Math.floor(Math.random() * data.people.length)
+        const imgs =  []
+        
+        data.people.forEach(img => {
+            imgs.push(img.image)    
+        })
+
+        onloadImg(imgs)
+
+        return data.people[randomNum]
+    } catch (error) {
+        console.error('Error fetching or processing data:', error)
+        throw error
+    }
 }
