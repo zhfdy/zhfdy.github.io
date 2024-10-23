c192c97690c5432519b5cf3917f715e705db929e
zhfdy <148465741+zhfdy@users.noreply.github.com>
2024-10-23 18:55:19 +0800

    更新 question.js

--- B卷/script/question.js
+++ B卷/script/question.js
@@ -2,13 +2,14 @@ import get from "./api.js"
 
 export default function question() {
     if (!(document.querySelector(".content_jjfa_list"))) return
-    if (document.querySelector(".content_jjfa_list div")) return
+    if (document.querySelector(".content_jjfa_list").children.length) return
     get('http://ncyunhua.com:9090/api/schoolBullying/schoolClass')
         .then(data => {
             const content_jjfa_list = document.querySelector(".content_jjfa_list")
-            data.forEach(({text} )=> {
+            data.forEach(({text}, index)=> {
                 const li = document.createElement("div")
                 li.textContent = text
+                li.classList.add(`content_jjfa_item${index}`)
                 content_jjfa_list.appendChild(li)
             })
         })
